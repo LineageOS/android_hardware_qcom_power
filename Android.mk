74db059350f2151cbd394b552fb25a4a09005d11
Steve Kondik <shade@chemlab.org>
2014-03-27 23:04:45 -0700

    power: This HAL is only for Google devices
    Change-Id: If50f9c72f6a336200bd1e3a4c5d1a0b6b54e0533

--- Android.mk
+++ Android.mk
@@ -1,6 +1,4 @@
-ifneq ($(filter msm8960 msm8974,$(TARGET_BOARD_PLATFORM)),)
-ifneq ($(TARGET_USES_CM_POWERHAL),true)
-ifneq ($(TARGET_PROVIDES_POWERHAL),true)
+ifneq ($(filter deb flo hammerhead mako,$(TARGET_DEVICE)),)
 ifneq ($(WITH_QC_PERF),true)
 
 LOCAL_PATH := $(call my-dir)
@@ -27,7 +25,5 @@ LOCAL_CFLAGS += -DTOUCH_BOOST
 endif
 
 include $(BUILD_SHARED_LIBRARY)
-endif
-endif
 endif
 endif
