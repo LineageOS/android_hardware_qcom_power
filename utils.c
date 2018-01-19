93359e81573168c56cd42278de86efef139a7ef1
dianlujitao <dianlujitao@lineageos.org>
2018-01-19 09:58:36 +0800

    power: Enable interaction boost unconditionally
    Change-Id: Idc38dadc081199dbf7c25df4e1bba65b2f3dde0c

--- utils.c
+++ utils.c
@@ -210,10 +210,6 @@ int is_interactive_governor(char* governor) {
    return 0;
 }
 
-#ifndef INTERACTION_BOOST
-void interaction(int UNUSED(duration), int UNUSED(num_args), int UNUSED(opt_list[]))
-{
-#else
 void interaction(int duration, int num_args, int opt_list[])
 {
     static int lock_handle = 0;
@@ -228,7 +224,6 @@ void interaction(int duration, int num_args, int opt_list[])
                 ALOGE("Failed to acquire lock.");
         }
     }
-#endif
 }
 
 int interaction_with_handle(int lock_handle, int duration, int num_args, int opt_list[])
