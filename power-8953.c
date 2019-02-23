007f8a8bcdd9308d23a48664700294656d4be580
dianlujitao <dianlujitao@lineageos.org>
2019-02-23 20:24:57 +0800

    power: Pass NULL parameter in powerHint if data is zero
     * This restores the behavior in AOSP and CAF power HAL to avoid
       confusion.
    
    Change-Id: I72f5bb9286e2f57121e39eea82d2fe8854989393

--- power-8953.c
+++ power-8953.c
@@ -95,8 +95,9 @@ int get_number_of_profiles()
 }
 #endif
 
-static int set_power_profile(int profile)
+static int set_power_profile(void *data)
 {
+    int profile = data ? *((int*)data) : 0;
     int ret = -EINVAL;
     const char *profile_name = NULL;
 
@@ -336,7 +337,7 @@ int power_hint_override(power_hint_t hint, void *data)
     int ret_val = HINT_NONE;
 
     if (hint == POWER_HINT_SET_PROFILE) {
-        if (set_power_profile(*(int32_t *)data) < 0)
+        if (set_power_profile(data) < 0)
             ALOGE("Setting power profile failed. perf HAL not started?");
         return HINT_HANDLED;
     }
